7cb18c9ceb26c3111aa3e77bc0679c0a8e3e4516
Maxence Chevee <max.rothen@hotmail.fr>
2024-01-04 18:00:39 +0100

    add blackjack

---
+++ frontend/src/components/GameStatus.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GameStatus = ({ playerTotal, dealerTotal }) => {
+  let status = "";
+
+  if (playerTotal > 21) {
+    status = "Le joueur a dépassé 21. C'est une défaite!";
+  } else if (dealerTotal > 21) {
+    status = "Le croupier a dépassé 21. C'est une victoire!";
+  } else if (playerTotal === dealerTotal) {
+    status = "Égalité!";
+  } else if (playerTotal === 21) {
+    status = "Blackjack! Le joueur a gagné!";
+  } else if (dealerTotal === 21) {
+    status = "Blackjack! Le croupier a gagné!";
+  } else if (
+    dealerTotal > 16 &&
+    dealerTotal < playerTotal &&
+    dealerTotal <= 21
+  ) {
+    status = "Le joueur a gagné!";
+  } else if (
+    playerTotal > 16 &&
+    playerTotal < dealerTotal &&
+    playerTotal <= 21
+  ) {
+    status = "Le croupier a gagné!";
+  } else {
+    status = "Personne n'a gagné.";
+  }
+
+  return <div>{status}</div>;
+};
+
+GameStatus.propTypes = {
+  playerTotal: PropTypes.number.isRequired,
+  dealerTotal: PropTypes.number.isRequired,
+};
+
+export default GameStatus;
