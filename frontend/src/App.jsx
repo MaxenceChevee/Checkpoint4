09221b16818858081251baf5b9ececf4f155d7e6
Maxence Chevee <max.rothen@hotmail.fr>
2024-01-06 00:17:08 +0100

    finish logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+// App.jsx
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { useAuth } from "./context/AuthContext"; // Importez le hook useAuth
 import "./styles/Global.scss";
 
 function App() {
+  const { isLoggedIn } = useAuth(); // Récupérez l'état d'authentification
+
   return (
     <div className="all">
-      <Header />
-      <Outlet />
+      <Header isLoggedIn={isLoggedIn} /> <Outlet />
     </div>
   );
 }
