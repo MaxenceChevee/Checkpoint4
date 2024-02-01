db6ca8c4347a93f9b54c02926400cd7299feab08
Maxence Chevee <max.rothen@hotmail.fr>
2024-02-01 16:01:53 +0100

    rectif scss wheel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,25 +4,29 @@ import "../styles/Home.scss";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h2 className="home-title">Bienvenue dans notre casino en ligne</h2>
-      <p>
-        Plongez dans le monde palpitant du jeu et de l'excitation. Notre casino
-        en ligne vous offre une expérience unique où le divertissement et la
-        chance se rencontrent.
-      </p>
+    <>
+      <h1 className="site-name">CashCatalyst</h1>
 
-      <p>
-        Tentez votre chance pour remporter d'incroyables jackpots et profitez
-        d'une atmosphère immersive qui vous transportera directement dans
-        l'univers du jeu.
-      </p>
-      <Link to="/games">
-        <button className="home-button casino-animation" type="button">
-          Commencer à jouer
-        </button>
-      </Link>
-    </div>
+      <div className="home-container">
+        <h2 className="home-title">Bienvenue dans notre casino en ligne</h2>
+        <p>
+          Plongez dans le monde palpitant du jeu et de l'excitation. Notre
+          casino en ligne vous offre une expérience unique où le divertissement
+          et la chance se rencontrent.
+        </p>
+
+        <p>
+          Tentez votre chance pour remporter d'incroyables jackpots et profitez
+          d'une atmosphère immersive qui vous transportera directement dans
+          l'univers du jeu.
+        </p>
+        <Link to="/games">
+          <button className="home-button casino-animation" type="button">
+            Commencer à jouer
+          </button>
+        </Link>
+      </div>
+    </>
   );
 };
 
